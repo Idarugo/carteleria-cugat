e43edc7b4cf95d9409b1f8a9aaacadd69c5aaa8b
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2026-06-26 12:45:44 -0400

    Mejoras sistema de cartelería calibrando fecha x4-4

--- components/carteles/cartel.tsx
+++ components/carteles/cartel.tsx
@@ -219,7 +219,7 @@ function CartelX4({ p }: { p: Producto }) {
   desde={p.desde}
   hasta={p.hasta}
   modo="x4"
-  className="mx-auto mt-[5px] w-[205px] font-mono text-[8px] font-bold italic leading-none"
+  className="mx-auto mt-[25px] w-[205px] font-mono text-[8px] font-bold italic leading-none"
 />
       </div>
     </div>
