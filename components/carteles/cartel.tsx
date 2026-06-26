ee0098d833bc4573734e193cce2928bc733c0e22
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2026-06-26 12:26:58 -0400

    Mejoras sistema de cartelería 1.3

--- components/carteles/cartel.tsx
+++ components/carteles/cartel.tsx
@@ -219,7 +219,7 @@ function CartelX4({ p }: { p: Producto }) {
   desde={p.desde}
   hasta={p.hasta}
   modo="x4"
-  className="mx-auto mt-[12px] w-[205px] font-mono text-[8px] font-bold italic leading-none"
+  className="mx-auto mt-[15px] w-[205px] font-mono text-[8px] font-bold italic leading-none"
 />
       </div>
     </div>
